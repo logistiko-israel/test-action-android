56f52d6cec1b0af337110f8fc457e48ac088642d
Israel de la Barrera <israel.barrera@logistiko.es>
2024-09-04 14:43:36 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'test-android-build',
+  webDir: 'www'
+};
+
+export default config;
